8394e386b65e3f3dbe68bf5c8dff3740f8edd65a
Leandro Oliveira <36287525+leandromagol@users.noreply.github.com>
2019-12-08 18:26:45 -0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <!-- <link rel="stylesheet" href="https://getbootstrap.com/docs/4.4/examples/dashboard/dashboard.css" > -->
         <!-- Fonts -->
-        <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/lte.css') }}">
         
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
